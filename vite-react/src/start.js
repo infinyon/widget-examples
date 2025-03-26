b9db72a29d2c206742bb929b22996216edb6e10b
Carson Rajcan <carajcan@gmail.com>
2025-03-26 18:23:24 -0500

    Update web component name

--- vite-react/src/start.js
+++ vite-react/src/start.js
@@ -4,4 +4,4 @@ import './index.css'
 import App from './App.jsx'
 
 document.getElementById("root").innerHTML =
-    "<infinyon-vite-react-template-0-0-0 ws-gateway-url='wss://ws.coincap.io/prices?assets=bitcoin'></infinyon-vite-react-template-0-0-0>";
+    "<infinyon-vite-react-template-0-0-1 ws-gateway-url='wss://ws.coincap.io/prices?assets=bitcoin'></infinyon-vite-react-template-0-0-1>";
