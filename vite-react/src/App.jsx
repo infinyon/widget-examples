479a70d0fa53db3494e654c4fb354bce28ef894d
Carson Rajcan <carajcan@gmail.com>
2025-03-26 17:16:47 -0500

    wip

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <>
+        <div id="main">
             <div>
                 <a href="https://vite.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -30,7 +30,7 @@ function App() {
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
             </p>
-        </>
+        </div>
     );
 }
 
@@ -46,9 +46,6 @@ if (!customElements.get("my-react-app")) {
               <style>
                 ${styleText}
               </style>
-              <div>
-                <p>Hello World</p>
-              </div>
             `;
         }
 
