010fcbbda52d10baaf2d65045d4bdb2cd8a48e42
Carson Rajcan <carajcan@gmail.com>
2025-03-26 18:33:45 -0500

    import from index.html

--- vite-react/src/start.js
+++ vite-react/src/start.js
@@ -2,6 +2,3 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-document.getElementById("root").innerHTML =
-    "<infinyon-vite-react-template-0-0-1 ws-gateway-url='wss://ws.coincap.io/prices?assets=bitcoin'></infinyon-vite-react-template-0-0-1>";
