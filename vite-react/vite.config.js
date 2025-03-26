8f747eea5a20b92628d79e0e912e3fbcf854e795
Carson Rajcan <carajcan@gmail.com>
2025-03-26 17:51:23 -0500

    Rename static assets

--- vite-react/vite.config.js
+++ vite-react/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/start.js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        }
+    }
 })
